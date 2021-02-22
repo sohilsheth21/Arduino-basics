9a384722c216ee1c6fb81ce8833b7176a67a05e8
sohilsheth21 <78338111+sohilsheth21@users.noreply.github.com>
2021-02-22 19:58:36 +0530

    Create photoresistor_led_brightness.ino

---
+++ photoresistor_led_brightness.ino
@@ -0,0 +1,19 @@
+int sensePin =0;
+int ledPin =9;
+
+void setup(){
+  analogReference(DEFAULT);
+  
+  pinMode(ledPin, OUTPUT);
+  
+}
+
+void loop(){
+  
+  int val = analogReference(sensePin);
+  
+  val = constrain(val, 750, 900);
+  int ledLevel = map(val, 750, 900, 255, 0);
+  
+  analogWrite(ledPin, ledLevel);
+}
